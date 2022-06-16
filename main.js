56d8bdf9ddfe426980f02f07cff8eaedeca233ec
tejasyadav123 <86886166+tejasyadav123@users.noreply.github.com>
2022-06-16 14:56:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,28 @@
+function preload(){
+
+}
+function setup(){
+    canvas = createCanvas(300 , 300)
+    canvas.center()
+    video = createCapture(VIDEO)
+    video.hide()
+    video.size(300 , 300)
+
+    posenet=ml5.poseNet(video , modelloaded)
+    posenet.on("pose",gotposes)
+}
+
+function draw(){
+image (video , 0 , 0 , 300 , 300)
+}
+
+function modelloaded(){
+    console.log("poseNetloaded")
+}
+function gotposes(result){
+    if(result.length > 0){
+     console.log(result)
+     console.log("nose x =" + result[0].pose.nose.x)
+     console.log("nose y =" + result[0].pose.nose.y)
+    }
+}
